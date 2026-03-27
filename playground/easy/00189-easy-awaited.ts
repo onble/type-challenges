ccda09c337756a78e794cfa3536c47f49f1f0cfc
lihaobo <>
2026-03-27 15:56:42 +0800

    PromiseLike

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,7 +22,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends PromiseLike<infer A> ? A extends PromiseLike<any> ? MyAwaited<A> : A : never
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
